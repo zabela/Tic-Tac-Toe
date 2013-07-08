c4284629fe4d409ac2b6ac81ceb65cb863103892
zabela <misabelpovoa@gmail.com>
2013-07-08 10:42:16 +0200

    2 classes

---
+++ Jogador.java
@@ -0,0 +1,21 @@
+package aula8;
+
+public class Jogador {
+
+	private char jog;
+
+	public Jogador(char j){
+		this.jog=j;
+	}
+
+	public char getJogador(){
+		return jog;
+	}
+
+	public void nextPlayer(){
+		if(this.jog=='X')
+			this.jog='O';
+		else
+			this.jog='X';
+	}
+}
